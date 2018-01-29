c4c6c41507e26591474c0c1470aa3b0918ddeef8
rsmcraft <randy@rsmellow.com>
2018-01-29 00:10:26 -0500

    add initial card logic

--- assets/script/site.js
+++ assets/script/site.js
@@ -1,4 +1,48 @@
-$(() => {
-	$('#game-board').append('<h2>Woriking...</h2>');
-});
+'use strict'
 
+$(() => {
+  Deck()
+
+  function sameColor (a, b) {
+    return Math.max(a, b) - Math.min(a, b) === 1
+  }
+
+  const suits = {
+    clubs: 100,
+    spades: 101,
+    hearts: 200,
+    diamonds: 201
+  }
+
+  const cards = {
+    nine: 9,
+    ten: 10,
+    jack: 11,
+    queen: 12,
+    king: 13,
+    ace: 14
+  }
+
+  function Card (suit, value) {
+    this.suit = suit
+    this.value = value
+    this.isLeft = function (trumpSuit) {
+      return this.value === cards.jack &&
+                   this.suit !== trumpSuit &&
+                   sameColor(this.suit, trumpSuit)
+    }
+
+    this.isRight = function (trumpSuit) {
+      return this.value === cards.jack &&
+                   this.suit === trumpSuit
+    }
+  }
+
+  function Deck () {
+    const arr = []
+    for (let s in suits) {
+      for (let c in cards) { arr.push(new Card(suits[s], cards[c])) }
+    }
+    console.log(arr)
+  }
+})
